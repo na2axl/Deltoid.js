2660c268590303d67a331f59a7ab30991aa5f847
Nana Axel <ax.lnana@outlook.com>
2017-06-13 10:55:06 +0100

    Fix path to Deltoid

--- tests/test-node.js
+++ tests/test-node.js
@@ -1,4 +1,4 @@
-var Deltoid = require('../deltoid');
+var Deltoid = require('../src/deltoid');
 
 var delta = {
     ops: [
@@ -29,7 +29,7 @@ var d2 = new Deltoid(delta, {
 var parsed1 = d1.parse();
 var parsed2 = d2.parse();
 
-console.log(parsed1.toHTML()); // => <strong><u>Hello </u></strong><em>world!</em>
+console.log(parsed1.toHTML()); // => <div id="line-1"><u><b>Hello </b></u><i>world!</i></div>
 console.log(parsed1.toPlainText()); // => Hello world!
-console.log(parsed2.toHTML()); // => <strong><u>Hello </u></strong><em>world!</em>
+console.log(parsed2.toHTML()); // => <div id="line-1"><u><strong>Hello </strong></u><em>world!</em></div>
 console.log(parsed2.toPlainText()); // => Hello world!
